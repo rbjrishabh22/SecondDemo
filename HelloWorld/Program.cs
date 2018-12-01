bf020798810d39c60ae4d269280a4604d2ffc010
Rishabh Bhardwaj <rbjrishabh22@gmail.com>
2018-12-01 16:26:06 +0530

    First commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line 1 code committed ");
+
+
+            //Don't delete below line
+            Console.ReadLine();
+        }
+    }
+}
